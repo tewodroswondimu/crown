eb5bf868676fc2b0abf0511f2f19eff26a5506dd
Tewodros Wondimu <tewodroswondimu@gmail.com>
2021-02-13 09:11:50 +0300

    created separate components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import HomePage from './components/homepage/homepage.component';
+
+import HomePage from './pages/homepage/homepage.component'
 
 function App() {
   return (
     <div>
       <HomePage />
-      Hello World
     </div>
   );
 }
