b5f938fe709765eed38aa0a650c784c0630d6721
Tewodros Wondimu <tewodroswondimu@gmail.com>
2021-02-13 13:02:21 +0300

    updated our menu-item to navigate withRouter higher order component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATS
+    </h1>
+  </div>
+); 
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/> 
+      <Route path='/hats' component={HatsPage}/>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,23 +1,26 @@
 import React from 'react'; 
 
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss'; 
 
-const MenuItem = ({ title, imageurl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return (
-        <div className={`${size} menu-item`}>
-            <div className='background-image' style={{
-                backgroundImage: `url(${imageurl})`
-            }}/>
-            <div className='content'>
-                <h1 className='title'>
-                    { title.toUpperCase() }
-                </h1>
-                <span>
-                    SHOP NOW
-                </span>
-            </div>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+                <div className='background-image' style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}/>
+                <div className='content'>
+                    <h1 className='title'>
+                        { title.toUpperCase() }
+                    </h1>
+                    
+                    <span>
+                        SHOP NOW
+                    </span>
+                </div>
         </div>
     )
 }
 
-export default MenuItem; 
+export default withRouter(MenuItem);
