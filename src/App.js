dc6259c214d520d5ad0c2ca129250e090833303d
Tewodros Wondimu <tewodroswondimu@gmail.com>
2021-02-13 17:12:25 +0300

    Added sign in form without the style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
-import Header from './components/header/header.component'
-import HomePage from './pages/homepage/homepage.component'
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/> 
-        <Route path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/> 
+        <Route path='/signin' component={SignInAndSignUp}/>
       </Switch> 
     </div>
   );
---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import './sign-in.style.scss'; 
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '', 
+            password: '', 
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        this.setState({
+            email: '', 
+            password: ''
+        })
+    }
+
+    handleChange = ({ name, value }) => {
+
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h1>I already have an account</h1>
+                <span>Sign in with your email and password</span> 
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email" name="email" onChange={this.handleChange} value={this.state.email} required />
+                    <label>Email</label>
+                    <input type="password" name="password" onChange={this.handleChange} value={this.state.password} required/>
+                    <label>Password</label>
+                    <input type="submit" value="Submit Form" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn; 
